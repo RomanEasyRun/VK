f4e4a06bdffc6ff4a16d82e6a472c38a9613f211
Roman Melnikov <Romaneasyrun@yandex.ru>
2021-04-24 17:40:07 +0700

    V1

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -2,11 +2,8 @@ package domain;
 
 public class Post {
     private int id;
-    private int ownerId;
-    private int fromId;
     private int date;
     private String text;
-    private int replyOwnerId;
     private int createdBy;
     private int replyPostId;
     private boolean friendsOnly;
@@ -17,9 +14,8 @@ public class Post {
     private boolean canDelete;
     private boolean canEdit;
     private boolean isPinned;
-    private boolean markedAsAds;
     private boolean isFavorite;
-    private int postponedId;
+
 
 
 }
